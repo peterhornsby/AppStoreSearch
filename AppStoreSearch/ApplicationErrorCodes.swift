f7088955b4f7b9d8fadaf2ccd03efe53d7c47c58
Peter Hornsby <me@peterhornsby.com>
2022-03-08 16:08:08 -0800

    inital setup of error codes

---
+++ AppStoreSearch/ApplicationErrorCodes.swift
@@ -0,0 +1,21 @@
+//
+//  ApplicationErrorCodes.swift
+//  AppStoreSearch
+//
+//  Created by Peter hornsby on 3/8/22.
+//
+
+import Foundation
+
+// application Errors
+let OkayNoErrorCode = 0
+let EmptyStringErrorCode = 1
+let nilStringErrorCode = 2
+let URLEncodingErrorCode = 3
+
+
+// Network Errors
+let Http200OKErrorCode = 200
+let NetworkUnreachableErrorCode = 50
+let NetworkLossConnectiveEerorCode = 51
+
