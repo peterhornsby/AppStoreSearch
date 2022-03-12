0c0fff2a3ab5f687bd383c4d9cbd25b35ff61d1a
Peter Hornsby <me@peterhornsby.com>
2022-03-11 17:12:26 -0800

    user interface: filter options

---
+++ AppStoreSearch/application/view/Filter/FilterOptionsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FilterOptionsViewController.swift
+//  AppStoreSearch
+//
+//  Created by Peter hornsby on 3/11/22.
+//
+
+import UIKit
+
+class FilterOptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
